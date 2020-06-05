42fb8fd12a0e2b8e668be926995b8243fbd5f777
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-06-05 10:20:34 +0200

    fix beerOrder status

--- src/main/java/it/frigir/msscbeerorderservice/web/model/BeerOrderStatusEnum.java
+++ src/main/java/it/frigir/msscbeerorderservice/web/model/BeerOrderStatusEnum.java
@@ -17,6 +17,8 @@
 
 package it.frigir.msscbeerorderservice.web.model;
 
-public enum OrderStatusEnum {
-    NEW, READY, PICKED_UP
+public enum BeerOrderStatusEnum {
+    NEW, VALIDATED, VALIDATION_EXCEPTION, ALLOCATED,
+    ALLOCATION_EXCEPTION, PENDING_INVENTORY, PICKED_UP,
+    DELIVERED, DELIVERY_EXCEPTION
 }
