cc9773a057ee6da83ceed01d5cac5da810dbb42d
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-06-06 10:52:17 +0200

    refactor order status and event

--- src/main/java/it/frigir/msscbeerorderservice/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/it/frigir/msscbeerorderservice/sm/BeerOrderStateMachineConfig.java
@@ -35,7 +35,7 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
     public void configure(StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions) throws Exception {
         transitions.withExternal()
                 .source(BeerOrderStatusEnum.NEW)
-                .target(BeerOrderStatusEnum.NEW)
+                .target(BeerOrderStatusEnum.VALIDATION_PENDING)
                 .event(BeerOrderEventEnum.VALIDATE_ORDER)
                 .and().withExternal()
                 .source(BeerOrderStatusEnum.NEW)
