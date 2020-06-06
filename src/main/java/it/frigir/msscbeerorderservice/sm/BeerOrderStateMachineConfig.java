0e74e329610edc500a92fbd66a02f2c65c29a4f7
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-06-06 11:22:32 +0200

    validate action

--- src/main/java/it/frigir/msscbeerorderservice/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/it/frigir/msscbeerorderservice/sm/BeerOrderStateMachineConfig.java
@@ -2,8 +2,10 @@ package it.frigir.msscbeerorderservice.sm;
 
 import it.frigir.msscbeerorderservice.domain.BeerOrderEventEnum;
 import it.frigir.msscbeerorderservice.domain.BeerOrderStatusEnum;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
@@ -15,9 +17,12 @@ import org.springframework.statemachine.state.State;
 import java.util.EnumSet;
 
 @Slf4j
+@RequiredArgsConstructor
 @Configuration
 @EnableStateMachineFactory
 public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    private final Action<BeerOrderStatusEnum, BeerOrderEventEnum> validateOrderAction;
 
     @Override
     public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
@@ -37,12 +42,13 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
                 .source(BeerOrderStatusEnum.NEW)
                 .target(BeerOrderStatusEnum.VALIDATION_PENDING)
                 .event(BeerOrderEventEnum.VALIDATE_ORDER)
+                .action(validateOrderAction)
                 .and().withExternal()
-                .source(BeerOrderStatusEnum.NEW)
+                .source(BeerOrderStatusEnum.VALIDATION_PENDING)
                 .target(BeerOrderStatusEnum.VALIDATED)
                 .event(BeerOrderEventEnum.VALIDATION_PASSED)
                 .and().withExternal()
-                .source(BeerOrderStatusEnum.NEW)
+                .source(BeerOrderStatusEnum.VALIDATION_PENDING)
                 .target(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
                 .event(BeerOrderEventEnum.VALIDATION_FAILED);
     }
